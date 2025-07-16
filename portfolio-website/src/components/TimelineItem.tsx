5a6a94398f0733d8067fcf233e94d19ed10b4930
cRazyeye19 <jlesterpansoy@gmail.com>
2025-07-16 09:49:01 +0800

    refactor(experience): refine timeline and timeline item components

--- portfolio-website/src/components/TimelineItem.tsx
+++ portfolio-website/src/components/TimelineItem.tsx
@@ -12,7 +12,7 @@ const TimelineItem: React.FC<TimelineItemProps> = React.memo(({ item, index }) =
       <div className="z-10 bg-gray-800 shadow-xl w-8 h-8 rounded-full hidden md:flex md:items-center">
         <h1 className="mx-auto font-semibold text-lg text-white">{index + 1}</h1>
       </div>
-      <div className="bg-gray-100 rounded-lg shadow-xl w-10/12 px-6 py-4 md:ml-8">
+      <div className="bg-white grow md:grow-0 rounded-lg shadow-sm w-10/12 px-6 py-4 md:ml-8">
         <h3 className="mb-3 font-bold text-gray-800 text-xl">{item.title}</h3>
         <p className="text-sm font-medium text-gray-600">{item.company}</p>
         <p className="text-sm text-gray-500">{item.startDate} - {item.endDate}</p>
